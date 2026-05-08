e595a989ecbf0fbfed260969ef6c781cd0b4841b
오종화 <ohjh2769@medialog.co.kr>
2026-05-08 21:00:52 +0900

    [add][Day4-1] CORS 설정 고도화

--- src/main/java/com/example/summarize/SummarizeController.java
+++ src/main/java/com/example/summarize/SummarizeController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
 public class SummarizeController {
 
     private final ClaudeService claudeService;
